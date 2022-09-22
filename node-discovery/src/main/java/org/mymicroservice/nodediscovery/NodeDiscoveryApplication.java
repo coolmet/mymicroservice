af0981d6ac5d3ed2b50cbba01aa41f2e3a3d63ab
ETIYACORP\metehan.meral <metehan.meral@etiya.com>
2022-09-22 16:41:35 +0300

    conn

--- node-discovery/src/main/java/org/mymicroservice/nodediscovery/NodeDiscoveryApplication.java
+++ node-discovery/src/main/java/org/mymicroservice/nodediscovery/NodeDiscoveryApplication.java
@@ -2,10 +2,10 @@ package org.mymicroservice.nodediscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableConfigServer
+@EnableEurekaServer
 public class NodeDiscoveryApplication {
 
     public static void main(String[] args) {
