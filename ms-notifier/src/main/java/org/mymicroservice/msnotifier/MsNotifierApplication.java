416127334b07e5dffe4213b8f3d44cd8b8b969d8
ETIYACORP\metehan.meral <metehan.meral@etiya.com>
2022-09-26 09:11:47 +0300

    latest with rabbit-mq (ms-notifier)

---
+++ ms-notifier/src/main/java/org/mymicroservice/msnotifier/MsNotifierApplication.java
@@ -0,0 +1,13 @@
+package org.mymicroservice.msnotifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsNotifierApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsNotifierApplication.class, args);
+    }
+
+}
